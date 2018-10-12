4e259f43d75fb9aed9c87a1a96878fb7c084f827
Zhuguohua <zgh_email@163.com>
2018-10-12 22:11:36 +0800

    add vehicle module code

---
+++ VehicleModle/README.md
@@ -0,0 +1,2 @@
+# VehicleModulePathPlanning
+This repository consist of the vehicle module and study the path planing methods
---
+++ VehicleModle/Vehicle.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 11 22:58:52 2018
+
+@author: Zhuguohua
+"""
+
+import pandas as pd
